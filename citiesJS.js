51445d1e85269395e6fe9f773ec3817be767541e
Igor Golovchits <golovchts18@gmail.com>
2018-08-11 16:28:50 +0300

    Correct cities comparison, some updates in the code

--- citiesJS.js
+++ citiesJS.js
@@ -6,52 +6,89 @@ var cities = ["Melbourne", "Sydney", "Perth", "Adelaide", "Canberra",
 
 var usedVariants = [];
 var alarm = "";
-function myFunc(){
-    var cityName = document.getElementById('city');
+var firstEntry = 0;
+var lastTextBoxSymbol = "";
+var finalMessage = "Congratulation! Computer lost. You are the winner!";
 
-    cityName.addEventListener('keydown', function onEvent(event) {
+function myFunc(){
+    var textBoxCity = document.getElementById("city");
+
+    textBoxCity.addEventListener("keydown", function onEvent(event) {
         if (event.key === "Enter") {
-            for (var i = 0; i < cities.length; i++){
-                var firstCitiesSymbol = cities[i].substring(0,1);
-                var lastUsersSymbol = cityName.value.slice(-1);
-                if (lastUsersSymbol.toUpperCase() === firstCitiesSymbol.toUpperCase()){
-                    getVariants(cityName.value, 'user list');
-                    if (alarm != ""){
-                        alarm = "";
-                        return;
-                    }
-                    cityName.value = cities[i];
-                    cities.splice(i, 1);
-                    getVariants(cityName.value, 'computer list');
-                    break;
+            if (firstEntry === 0) {
+                if (textBoxCity.value === "") {
+                    alert("The city name can't be empty!");
+                }
+                else {
+                    compareCities(textBoxCity);
                 }
             }
+            else{
+                if (textBoxCity.value === "") {
+                    alert("The city name can't be empty!");
+                }
+                else {
+                    var firstTextBoxSymbol = textBoxCity.value.substring(0, 1);  //первая буква города из текстбокса
+
+                    if (firstTextBoxSymbol.toUpperCase() === lastTextBoxSymbol.toUpperCase()) {
+                        compareCities(textBoxCity);
+                    }
+                    else {
+                        alert("The city's name must start with a(an) \"" + lastTextBoxSymbol.toUpperCase() + "\" character. "  +
+                              "If you don't write it you will lose.");
+                    }
+                }
+            }
+            firstEntry++;
         }
     });
 }
 
-function getVariants(city, idName) {
-    var variants = [];
-    variants.push(city);
-    for (var i = 0; i < usedVariants.length; i++){
-        if (city === usedVariants[i]){
-            alarm = 'This city has already been used! Please enter another variant.';
-            alert(alarm);
-            return;
+function compareCities(textBoxCity) {
+    setCities(textBoxCity.value, "user list");           //добавление города в html лист пользователя
+    lastTextBoxSymbol = textBoxCity.value.slice(-1);         //последняя буква города из текстбокса
+
+    for (var i = 0; i < cities.length; i++) {
+        var firstCitiesSymbol = cities[i].substring(0, 1);      //первая буква города из массива
+
+        if (lastTextBoxSymbol.toUpperCase() === firstCitiesSymbol.toUpperCase()) {
+            // if (alarm !== ""){
+            //     alarm = "";
+            //     return;
+            // }
+            textBoxCity.value = cities[i];                         //запись в текстбокс города из массива
+            cities.splice(i, 1);
+            setCities(textBoxCity.value, "computer list");       //добавление города в html лист компьютера
+            lastTextBoxSymbol = textBoxCity.value.slice(-1);
+            break;
         }
     }
-    usedVariants.push(city);
-    addItemToList(variants, idName);
 }
 
-function addItemToList(variants, idName) {
+function setCities(city, idName) {
+    // for (var i = 0; i < usedVariants.length; i++){
+    //     if (city === usedVariants[i]){
+    //         alarm = "This city has already been used! Please enter another variant.";
+    //         alert(alarm);
+    //         return;
+    //     }
+    // }
+    usedVariants.push(city);
+
     var list = document.getElementById(idName);
+    var node = document.createElement("li");
+    var content = document.createTextNode(city)
 
-    for (var i = 0; i < variants.length; i++){
-        var node = document.createElement("li");
-        var content = document.createTextNode(variants[i]);
+    node.appendChild(content);
+    list.appendChild(node);
+}
+var i = 0;
 
-        node.appendChild(content);
-        list.appendChild(node);
+function writeFinalMessage() {
+
+    if (i < finalMessage.length) {
+        document.getElementById("winner").innerHTML += finalMessage.charAt(i);
+        i++;
+        setTimeout(writeFinalMessage, 50);
     }
 }
