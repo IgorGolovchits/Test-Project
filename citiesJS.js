4f9f4d5ad6ef741419d718abf2d0ecef647c43b9
Igor Golovchits <golovchts18@gmail.com>
2018-08-11 19:32:57 +0300

    Added: comparison for the same names, final message, code updates

--- citiesJS.js
+++ citiesJS.js
@@ -1,94 +1,118 @@
 var cities = ["Melbourne", "Sydney", "Perth", "Adelaide", "Canberra",
-    "Minsk", "Moscow", "Kiev", "Stockholm", "Oslo",
-    "Madrid", "Rome", "Milan", "Berlin", "Paris",
-    "Edinburgh", "Aberdeen", "Dublin", "York", "London",
-    "New York", "Washington", "Pittsburgh", "Detroit", "Ottawa"];
+              "Minsk", "Moscow", "Kiev", "Stockholm", "Oslo",
+              "Madrid", "Rome", "Milan", "Berlin", "Paris",
+              "Edinburgh", "Aberdeen", "Dublin", "York", "London",
+              "New York", "Washington", "Pittsburgh", "Detroit", "Ottawa"];
 
 var usedVariants = [];
 var alarm = "";
 var firstEntry = 0;
 var lastTextBoxSymbol = "";
-var finalMessage = "Congratulation! Computer lost. You are the winner!";
 
-function myFunc(){
+const FINAL_MESSAGE = "Congratulation! Computer lost. You are the winner!";
+
+function startTheGame(){
     var textBoxCity = document.getElementById("city");
 
     textBoxCity.addEventListener("keydown", function onEvent(event) {
         if (event.key === "Enter") {
-            if (firstEntry === 0) {
-                if (textBoxCity.value === "") {
-                    alert("The city name can't be empty!");
-                }
-                else {
+            if (firstEntry === 0) {   //первый вход в игру. проверяется сначала последняя буква в текстбоксе и первая в массиве городов
+                if (checkForEmptyName(textBoxCity) === true) return;
+
+                compareCities(textBoxCity);
+            } else { // второй и последующие входы проверяют на совпадение первую букву текстбокса и последнюю предыдущего слова
+                if (checkForEmptyName(textBoxCity) === true) return;
+
+                var firstTextBoxSymbol = textBoxCity.value.substring(0, 1);
+
+                if (firstTextBoxSymbol.toUpperCase() === lastTextBoxSymbol.toUpperCase()) {
                     compareCities(textBoxCity);
                 }
-            }
-            else{
-                if (textBoxCity.value === "") {
-                    alert("The city name can't be empty!");
-                }
                 else {
-                    var firstTextBoxSymbol = textBoxCity.value.substring(0, 1);  //первая буква города из текстбокса
-
-                    if (firstTextBoxSymbol.toUpperCase() === lastTextBoxSymbol.toUpperCase()) {
-                        compareCities(textBoxCity);
-                    }
-                    else {
-                        alert("The city's name must start with a(an) \"" + lastTextBoxSymbol.toUpperCase() + "\" character. "  +
-                              "If you don't write it you will lose.");
-                    }
+                    alert("The city's name must start with a(an) \"" + lastTextBoxSymbol.toUpperCase() + "\" character. " +
+                          "If you don't write it you will lose.");
                 }
             }
+
             firstEntry++;
         }
     });
 }
 
 function compareCities(textBoxCity) {
-    setCities(textBoxCity.value, "user list");           //добавление города в html лист пользователя
-    lastTextBoxSymbol = textBoxCity.value.slice(-1);         //последняя буква города из текстбокса
+    var finish = false;
 
-    for (var i = 0; i < cities.length; i++) {
-        var firstCitiesSymbol = cities[i].substring(0, 1);      //первая буква города из массива
+    setCities(textBoxCity.value, "user list");
 
-        if (lastTextBoxSymbol.toUpperCase() === firstCitiesSymbol.toUpperCase()) {
-            // if (alarm !== ""){
-            //     alarm = "";
-            //     return;
-            // }
-            textBoxCity.value = cities[i];                         //запись в текстбокс города из массива
-            cities.splice(i, 1);
-            setCities(textBoxCity.value, "computer list");       //добавление города в html лист компьютера
-            lastTextBoxSymbol = textBoxCity.value.slice(-1);
-            break;
+    if (checkAlarm() === true) return;
+
+    lastTextBoxSymbol = textBoxCity.value.slice(-1);
+
+    for (var i = 0; i <= cities.length; i++) {
+        if (i === cities.length){
+            finish = true;
+        } else {
+            var firstCitiesSymbol = cities[i].substring(0, 1);
+
+            if (lastTextBoxSymbol.toUpperCase() === firstCitiesSymbol.toUpperCase()) {
+                if (checkAlarm() === true) return;
+
+                textBoxCity.value = cities[i];
+                cities.splice(i, 1);
+
+                setCities(textBoxCity.value, "computer list");
+
+                lastTextBoxSymbol = textBoxCity.value.slice(-1);
+                break;
+            }
         }
+    }
+
+    if (finish === true){
+        writeFinalMessage();
+        textBoxCity.disabled = true;
     }
 }
 
 function setCities(city, idName) {
-    // for (var i = 0; i < usedVariants.length; i++){
-    //     if (city === usedVariants[i]){
-    //         alarm = "This city has already been used! Please enter another variant.";
-    //         alert(alarm);
-    //         return;
-    //     }
-    // }
+    for (var i = 0; i < usedVariants.length; i++){
+        if (city.toUpperCase() === usedVariants[i].toUpperCase()){
+            alarm = "This city has already been used! Please enter another variant. If you don't you will lose.";
+            alert(alarm);
+            return;
+        }
+    }
+
     usedVariants.push(city);
 
     var list = document.getElementById(idName);
     var node = document.createElement("li");
-    var content = document.createTextNode(city)
+    var content = document.createTextNode(city);
 
     node.appendChild(content);
     list.appendChild(node);
 }
-var i = 0;
+
+function checkForEmptyName(textBoxCity) {
+    if (textBoxCity.value === "") {
+        alert("The city's name can't be empty!");
+        return true;
+    }
+}
+
+function checkAlarm() {
+    if (alarm !== ""){
+        alarm = "";
+        return true;
+    }
+}
+
+var letterIndex = 0;
 
 function writeFinalMessage() {
-
-    if (i < finalMessage.length) {
-        document.getElementById("winner").innerHTML += finalMessage.charAt(i);
-        i++;
+    if (letterIndex < FINAL_MESSAGE.length) {
+        document.getElementById("winner").innerHTML += FINAL_MESSAGE.charAt(letterIndex);
+        letterIndex++;
         setTimeout(writeFinalMessage, 50);
     }
 }
